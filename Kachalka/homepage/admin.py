347e1d718935c53eebd84e1d79414367a7395bf0
Kirill Ryaska <ghettoboy2106@gmail.com>
2024-11-04 12:56:09 +0500

    before migrations

--- Kachalka/homepage/admin.py
+++ Kachalka/homepage/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Statistic, Types
+
+class StatisticAdmin(admin.ModelAdmin):
+    list_display = (
+        'date',
+        'calories',
+        'time',
+        'type',
+    )
+    list_editable = (
+        'date',
+        'calories',
+        'time',
+        'type',
+    )
+
+admin.site.register(Statistic)
+admin.site.register(Types)
