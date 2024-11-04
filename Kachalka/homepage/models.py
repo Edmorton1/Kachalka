8a7bf2913c2fe32a3377c490f04427366df91735
Kirill Ryaska <ghettoboy2106@gmail.com>
2024-11-04 11:57:06 +0500

    before DJDT

--- Kachalka/homepage/models.py
+++ Kachalka/homepage/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Types(models.Model):
+    name = models.CharField('Тип', max_length=32)
+
+class Statistic(models.Model):
+    date = models.DateField('Дата')
+    calories = models.IntegerField('Калории')
+    time = models.TimeField('Время')
+    type = models.ForeignKey(
+        Types,
+        on_delete=models.CASCADE,
+        related_name='types',
+    )
