a7ab6ec68d6be1f2568977cbcf67b56350530972
Kirill Ryaska <ghettoboy2106@gmail.com>
2024-11-07 13:23:20 +0500

    before timedelta

--- Kachalka/Kachalka/settings.py
+++ Kachalka/Kachalka/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'homepage.apps.HomepageConfig',
+    'about.apps.AboutConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
 ]
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- Kachalka/homepage/views.py
+++ Kachalka/homepage/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from .models import Statistic
+from django.db.models import Avg, Count
+import datetime
 
 # Create your views here.
+
+def average_time(a):
+    hours = 0
+    minutes = 0
+    for i in range(len(a)):
+        hours += a[i]['time'].hour
+        minutes += a[i]['time'].minute
+    return (int(hours / len(a) * 60 + minutes / len(a))//60, int(hours / len(a) * 60 + minutes / len(a)) % 60)
+
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    statis_list = Statistic.objects.values('date', 'calories', 'time', 'type__name')
+
+    context = {
+        'statis': statis_list,
+        'best_result': statis_list.values('date', 'calories', 'time'),
+        'count': statis_list.aggregate(Count('date')),
+        'average_calories': round(statis_list.aggregate(Avg('calories'))['calories__avg']),
+        'average_time': average_time(statis_list.values('time')),
+    }
+    return render(request, template, context)
