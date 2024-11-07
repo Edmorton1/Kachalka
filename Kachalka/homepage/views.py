0a6fe0bfdb6e29c848038a5f76af431b65e24f44
Kirill Ryaska <ghettoboy2106@gmail.com>
2024-11-07 14:44:55 +0500

    table 2

--- Kachalka/homepage/views.py
+++ Kachalka/homepage/views.py
@@ -13,13 +13,28 @@ def average_time(a):
         minutes += a[i]['time'].minute
     return (int(hours / len(a) * 60 + minutes / len(a))//60, int(hours / len(a) * 60 + minutes / len(a)) % 60)
 
+def best_result(a):
+    maximum = 0
+    date = 0
+    calories = 0
+    for i in a:
+        date = i['date']
+        time = i['time']
+        calories = i['calories']
+        maximum = max(maximum, calories)
+    for i in a:
+        if i['calories'] == maximum:
+            date = i['date']
+            time = i['time']
+            return (date, maximum, time)
+
 def index(request):
     template = 'homepage/index.html'
     statis_list = Statistic.objects.values('date', 'calories', 'time', 'type__name')
 
     context = {
         'statis': statis_list,
-        'best_result': statis_list.values('date', 'calories', 'time'),
+        'best_result': best_result(statis_list.values('date', 'calories', 'time')),
         'count': statis_list.aggregate(Count('date')),
         'average_calories': round(statis_list.aggregate(Avg('calories'))['calories__avg']),
         'average_time': average_time(statis_list.values('time')),
